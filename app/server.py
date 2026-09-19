a5cd6b220ec469241905026b12007817a5238cf7
Esakki Raja <esakkiraja2528@gmail.com>
2026-09-19 17:25:17 +0530

    Prepare V7 for cloud deployment

--- app/server.py
+++ app/server.py
@@ -420,12 +420,11 @@ if __name__ == "__main__":
     print("EsakkiAI is starting...")
     print()
     print(
-        "Open: http://127.0.0.1:5000"
-    )
-    print()
+    "EsakkiAI server is running."
+)
 
-    app.run(
-        host="127.0.0.1",
-        port=5000,
-        debug=False
-    )
+app.run(
+    host="0.0.0.0",
+    port=int(os.environ.get("PORT", 5000)),
+    debug=False
+)
